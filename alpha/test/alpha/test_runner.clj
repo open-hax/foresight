5445207845caabba91adb1f4bae6d8ea378544c5
Err <foamy125@gmail.com>
2026-08-13 19:06:26 -0500

    test(alpha): run diagram artifact suite

--- alpha/test/alpha/test_runner.clj
+++ alpha/test/alpha/test_runner.clj
@@ -1,5 +1,6 @@
 (ns alpha.test-runner
   (:require [alpha.law.artifact-test]
+            [alpha.law.diagram-artifact-test]
             [alpha.law.markdown.facet-test]
             [alpha.law.markdown.profile-test]
             [alpha.shape.mermaid-test]
@@ -7,6 +8,7 @@
 
 (defn -main [& _]
   (let [result (test/run-tests 'alpha.law.artifact-test
+                               'alpha.law.diagram-artifact-test
                                'alpha.law.markdown.profile-test
                                'alpha.law.markdown.facet-test
                                'alpha.shape.mermaid-test)]
